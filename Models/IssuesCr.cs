4026802e92cb815050cd218d62735a957036e67d
BonganiChauke <bonganichauke11@outlook.com>
2026-02-18 18:32:19 +0200

    Another model with properties that I will to create in the database

---
+++ Models/IssuesCr.cs
@@ -0,0 +1,12 @@
+﻿namespace Backend_Challenge.Models
+{
+    public class IssuesCr
+    {
+        // properties of the Issues model
+        public int id { get; set; }
+        public string title { get; set; } = string.Empty;
+        public string description { get; set; } = string.Empty;
+        public string status { get; set; } = string.Empty;
+        public DateTime createdAt { get; set; }
+    }
+}
