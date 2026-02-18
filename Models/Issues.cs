637866cb5b7c3a01c4b40a7e8cf6d6fa10ddd122
BonganiChauke <bonganichauke11@outlook.com>
2026-02-18 21:43:32 +0200

    Removed ID property as it should auto increament when records are added

--- Models/Issues.cs
+++ Models/Issues.cs
@@ -5,7 +5,6 @@ namespace Backend_Challenge.Models
     public class Issues
     {
         // properties of the Issues model
-        public int id { get; set; }
         public string title { get; set; } = string.Empty;
         public string description { get; set; } = string.Empty;
         public string status{ get; set; } = string.Empty;
