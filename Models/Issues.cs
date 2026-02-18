9d63466d762f78c88c1685b935e29f7c3bc01227
BonganiChauke <bonganichauke11@outlook.com>
2026-02-18 18:17:01 +0200

    A model class for issues properties

---
+++ Models/Issues.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace Backend_Challenge.Models
+{
+    public class Issues
+    {
+        // properties of the Issues model
+        public int id { get; set; }
+        public string title { get; set; } = string.Empty;
+        public string description { get; set; } = string.Empty;
+        public string status{ get; set; } = string.Empty;
+        public DateTime createdAt { get; set; }
+
+    }
+}
