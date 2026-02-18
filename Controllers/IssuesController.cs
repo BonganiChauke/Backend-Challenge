d103e466290b82cfea23382109ae2de0d7bef6e9
BonganiChauke <bonganichauke11@outlook.com>
2026-02-18 18:18:40 +0200

    controller with contructor to connect to database

---
+++ Controllers/IssuesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend_Challenge.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IssuesController : ControllerBase
+    {
+        // connection string for db
+        private readonly string _connectionString;
+
+        // constructor and dependencies can be injected
+        public IssuesController(IConfiguration configuration)
+        {
+            // initialize connection string from configuration
+            // if the connection string is not found, it will be set to an empty string
+            _connectionString = configuration.GetConnectionString("DefaultConnection") ?? "";
+
+
+
+        }
+    }
+}
