2308de01d98441297a0c2b4b1977c1f608698929
BonganiChauke <bonganichauke11@outlook.com>
2026-02-19 15:58:57 +0200

    Data annotations

--- Models/Issues.cs
+++ Models/Issues.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Http.HttpResults;
+using System.ComponentModel.DataAnnotations;
 
 namespace Backend_Challenge.Models
 {
     public class Issues
     {
         // properties of the Issues model
+        [Required]
+        [StringLength(100)]
         public string title { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(350)]
         public string description { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(50)]
         public string status{ get; set; } = string.Empty;
 
     }
